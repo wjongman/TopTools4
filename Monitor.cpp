7b503a12bfa8c0f560a4deb717e07de6db2f1343
Willem <willem@toptools.org>
2008-11-26 18:01:56 +0000

    Some refactoring and renaming

--- Monitor.cpp
+++ Monitor.cpp
@@ -25,40 +25,48 @@
 //  MONITOR_WORKAREA 0x0002        // use monitor work area
 //  MONITOR_AREA     0x0000        // use monitor entire area
 
-void ClipOrCenterRectToMonitor(LPRECT prc, UINT flags)
+void ClipOrCenterRectToMonitor(LPRECT prcGiven, UINT flags)
 {
-    HMONITOR hMonitor;
-    MONITORINFO mi;
-    RECT        rc;
-    int         w = prc->right  - prc->left;
-    int         h = prc->bottom - prc->top;
-
     // Get the nearest monitor to the passed rect.
-    hMonitor = MonitorFromRect(prc, MONITOR_DEFAULTTONEAREST);
+    HMONITOR hMonitor;
+    hMonitor = MonitorFromRect(prcGiven, MONITOR_DEFAULTTONEAREST);
 
     // Get the work area or entire monitor rect.
+    MONITORINFO mi;
     mi.cbSize = sizeof(mi);
     GetMonitorInfo(hMonitor, &mi);
 
+    // Get clipping-rect we are interested in
+    RECT rcClip;
     if (flags & MONITOR_WORKAREA)
-        rc = mi.rcWork;
-    else
-        rc = mi.rcMonitor;
-
-    // Center or clip the passed rect to the monitor rect
-    if (flags & MONITOR_CENTER)
     {
-        prc->left   = rc.left + (rc.right  - rc.left - w) / 2;
-        prc->top    = rc.top  + (rc.bottom - rc.top  - h) / 2;
-        prc->right  = prc->left + w;
-        prc->bottom = prc->top  + h;
+        rcClip = mi.rcWork;
     }
     else
     {
-        prc->left   = max(rc.left, min(rc.right-w,  prc->left));
-        prc->top    = max(rc.top,  min(rc.bottom-h, prc->top));
-        prc->right  = prc->left + w;
-        prc->bottom = prc->top  + h;
+        rcClip = mi.rcMonitor;
+    }
+
+    // Get dimensions of given rect
+    int w = prcGiven->right  - prcGiven->left;
+    int h = prcGiven->bottom - prcGiven->top;
+
+    // Calculate corrected dimensions for given rect
+    if (flags & MONITOR_CENTER)
+    {
+        // Center given rect to the monitor rect
+        prcGiven->left   = rcClip.left + (rcClip.right  - rcClip.left - w) / 2;
+        prcGiven->top    = rcClip.top  + (rcClip.bottom - rcClip.top  - h) / 2;
+        prcGiven->right  = prcGiven->left + w;
+        prcGiven->bottom = prcGiven->top  + h;
+    }
+    else
+    {
+        // Clip given rect to the monitor rect
+        prcGiven->left   = max(rcClip.left, min(rcClip.right  - w, prcGiven->left));
+        prcGiven->top    = max(rcClip.top,  min(rcClip.bottom - h, prcGiven->top));
+        prcGiven->right  = prcGiven->left + w;
+        prcGiven->bottom = prcGiven->top  + h;
     }
 }
 
