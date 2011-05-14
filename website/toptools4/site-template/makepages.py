80b50103b2a46e6fb437a66aa3b92d86d70d9183
Willem <willem@toptools.org>
2011-05-14 15:27:19 +0000

    ConfigObj and MarkDown seem to work well

--- website/toptools4/site-template/makepages.py
+++ website/toptools4/site-template/makepages.py
@@ -1,7 +1,8 @@
 #!C:/Python27/python.exe
 
 from stringtemplate3 import StringTemplateGroup
-import ConfigParser
+from configobj import ConfigObj
+import markdown
 
 ##-----------------------------------------------------------------------------
 class MenuItemDescriptor(object):
@@ -9,14 +10,13 @@ class MenuItemDescriptor(object):
         self.id = id
         self.caption = ''
         self.pageurl = ''
-        self.downloadurl = ''
         self.selected = False
+        self.submenu = []
 
 ##-----------------------------------------------------------------------------
 def render_all_pages():
-    config = ConfigParser.ConfigParser()
-    config.read('./content/content.ini')
-    pagenames = config.sections()
+    config = ConfigObj('./content/content.ini')
+    pagenames = config.sections
     for pagename in pagenames:
         page = render_page(config, pagename)
         f = open(pagename, 'w+')
@@ -25,32 +25,46 @@ def render_all_pages():
 
 ##-----------------------------------------------------------------------------
 def render_page(config, pagename):
-
     stgroup = StringTemplateGroup("groupName", "./templates")
     st = stgroup.getInstanceOf("index")
 
-    st["title"] = config.get(pagename, 'title')
-    st["content"] = config.get(pagename, 'text')
-    st["subtitle"] = config.get(pagename, 'subtitle')
+    st["title"] = config[pagename]['title']
+    content = config[pagename]['text']
+    html = markdown.markdown(content)
+    st["content"] = html
+#    st["content"] = config[pagename]['text']
+    st["subtitle"] = config[pagename]['subtitle']
     st["menuitems"] = get_menuitems(config, pagename)
-
+#    submenuitems =
     return str(st)
 
 ##-----------------------------------------------------------------------------
 def get_menuitems(config, pagename):
     menuitems = []
 
-    sectionnames = config.sections()
+    sectionnames = config.sections
     for sectionname in sectionnames:
         md = MenuItemDescriptor(sectionname)
         md.pageurl = sectionname
-        md.caption = config.get(sectionname, 'title')
-        md.downloadurl = config.get(sectionname, 'downloadurl')
+        md.caption = config[sectionname]['title']
         md.selected = (sectionname == pagename)
-
+        md.submenu = get_submenuitems(config, sectionname)
         menuitems.append(md)
 
     return menuitems
+
+##-----------------------------------------------------------------------------
+def get_submenuitems(config, sectionname):
+    submenuitems = []
+
+    submenus = config[sectionname]['submenu']
+    for submenu in submenus:
+        smd = MenuItemDescriptor(submenu)
+        smd.caption = submenu
+        smd.pageurl = submenus[submenu]
+        submenuitems.append(smd)
+
+    return submenuitems
 
 ##-----------------------------------------------------------------------------
 def main():
